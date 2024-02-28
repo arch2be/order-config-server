80f7e18612d57dcfd2fae09e884b8ec4625a2023
Mateusz <jdvozef@gmail.com>
2024-02-28 20:44:40 +0100

    Initial commit

---
+++ src/main/java/io/github/arch2be/orderconfigserver/OrderConfigServerApplication.java
@@ -0,0 +1,13 @@
+package io.github.arch2be.orderconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderConfigServerApplication.class, args);
+    }
+
+}
